46e53f9e81583530353e24fd72c998f2c4ef72b8
Robert Marczak <robert.m.1305@gmail.com>
2018-04-29 20:33:41 +0100

    Added __construct, addName, addAge, addLearning, and returnPerson methods in Array_example.php

--- src/Array_example.php
+++ src/Array_example.php
@@ -4,10 +4,34 @@ namespace src;
 
 /**
  * Array example
+ * Any logic that gets the data from a file or database
  */
 class Array_example
 {
-  public function print(){
-    echo 'Print from Array_example';
+
+  private $person;
+
+  public function __construct()
+  {
+    $this->person = [];
+  }
+
+  public function addName($name = 'Robert')
+  {
+    $this->person['name'] = $name;
+  }
+
+  public function addAge($age = 34)
+  {
+    $this->person['age'] = $age;
+  }
+
+  public function addLearning($learning = true)
+  {
+    $this->person['learning'] = $learning;
+  }
+  public function returnPerson()
+  {
+    return $this->person;
   }
 }
