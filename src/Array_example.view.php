6017ed1eabb182f9c520856ecfd9fa048abee310
Robert Marczak <robert.m.1305@gmail.com>
2018-04-29 18:22:16 +0100

    Added unsorted list with foreach loop in Array_example.view.php

--- src/Array_example.view.php
+++ src/Array_example.view.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>The HTML5 Herald</title>
+  <title>The Array Example</title>
   <meta name="description" content="Array Example">
   <meta name="author" content="Robert Marczak">
   <link rel="stylesheet" href="css/styles.css">
@@ -12,6 +12,11 @@
 </head>
 
 <body>
-  <strong>Array Example</strong><br/>
+  <header><strong>Array Example</strong><br/></header>
+  <ul>
+    <?php foreach ($person as $key => $value) :?>
+      <li><strong><?= $key ?></strong><?= ' => ' .  $value ?>
+    <?php endforeach;?>
+  </ul>
 </body>
 </html>
