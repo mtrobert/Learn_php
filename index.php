6ae643fe2ad0ec744c12a914f1486bcb7e1f09ef
Robert Marczak <robert.m.1305@gmail.com>
2018-04-29 20:34:38 +0100

    git reset

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
 require 'vendor/autoload.php';
+
+
+
+$name = 'Jonathan';  //data from a off-class
+$age = 33;  //data from a off-class
+$age = false;  //data from a off-class
+
+$futureEmployee = new src\Array_example();
+$futureEmployee->addName();
+$futureEmployee->addAge();
+$futureEmployee->addLearning();
+
+$person = $futureEmployee->returnPerson();
+
+require 'src/Array_example.view.php';
