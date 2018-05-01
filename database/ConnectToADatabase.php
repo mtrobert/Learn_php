232a8815e8582cd01ee7f73c38558ec567ea6381
Robert Marczak <robert.m.1305@gmail.com>
2018-05-01 12:22:48 +0100

    Added connection to a database using PDO and try catch in ConnectToADatabase.php

--- database/ConnectToADatabase.php
+++ database/ConnectToADatabase.php
@@ -1,1 +1,10 @@
-<?php 
+<?php
+
+try {
+
+  $pdo = new PDO('mysql:host=localhost;dbname=learning', 'root', '');
+
+} catch (PDOException $e) {
+  // var_dump($e);
+  die('Could not connect to a database. Please try again.');
+}
