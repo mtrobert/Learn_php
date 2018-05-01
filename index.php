aab160a289e79509316011737a7be80a0f162e4d
Robert Marczak <robert.m.1305@gmail.com>
2018-05-01 12:33:24 +0100

    Fetched data from database

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'database/ConnectToADatabase.php';
+
+$query = $pdo->prepare('select * from users');
+$query->execute();
+$results = $query->fetchAll();
+echo '<pre>', var_dump($results), '</pre>';
 
 
 
-$name = 'Jonathan';  //data from a off-class
-$age = 33;  //data from a off-class
-$age = false;  //data from a off-class
+// $name = 'Jonathan';  //data from a off-class
+// $age = 44;  //data from a off-class
+// $learning = true;  //data from a off-class
 
 $futureEmployee = new src\Array_example();
 $futureEmployee->addName();
