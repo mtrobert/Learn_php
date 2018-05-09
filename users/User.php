a0eb5b7ef0c95387328c4e346a4d0f62dce2d3a9
Robert Marczak <robert.m.1305@gmail.com>
2018-05-09 09:50:17 +0100

    Created User.php file where properties represent fields from a users table in a database

---
+++ users/User.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Class containing methods and properties for a user
+ */
+class User
+{
+  protected $name, $age, $learning, $id;
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function getAge(){
+    return $this->age;
+  }
+
+  public function getLearning(){
+    return $this->learning;
+  }
+
+  public function getId(){
+    return $this->id;
+  }
+
+}
