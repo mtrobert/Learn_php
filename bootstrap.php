7cb5711d22958cbe95eda190c4057f39de625c84
Robert Marczak <robert.m.1305@gmail.com>
2018-05-15 13:57:24 +0100

    Set config variable to require config.php so the ['database'] can be passed as an argument

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,8 @@
 
 require 'database/ConnectToADatabase.php';
 require 'database/QuerieBuilder.php';
+$config = require 'config.php';
 
-return new QuerieBuilder(ConnectToADatabase::ConnectToDb());
+return new QuerieBuilder(
+  ConnectToADatabase::ConnectToDb($config['database'])
+);
