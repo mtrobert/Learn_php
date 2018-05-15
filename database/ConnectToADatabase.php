bb0042367ab06a8f04c9a2e7a7959ed671e9cb6b
Robert Marczak <robert.m.1305@gmail.com>
2018-05-15 14:28:55 +0100

    Replaced hardcoded values for PDO with an config array from config.php in ConnectToADatabase.php

--- database/ConnectToADatabase.php
+++ database/ConnectToADatabase.php
@@ -5,11 +5,15 @@
 class ConnectToADatabase
 {
 
-  public static function ConnectToDb(){           //static method for easy access
+  public static function ConnectToDb($config){           //static method for easy access
     try {
 
-      return $pdo = new PDO('mysql:host=localhost;dbname=learning', 'root', '');
-
+      return  new PDO(
+        $config['connection'] . ';dbname=' . $config['name'],
+        $config['username'],
+        $config['password'],
+        $config['options']
+      );
     } catch (PDOException $e) {
 
       die($e->getMessage() . "</br></br>" . 'Could not connect to a database. Please try again.');
