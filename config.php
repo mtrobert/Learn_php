5bca738b8e04be440a97d4b9f702ac09783d46bf
Robert Marczak <robert.m.1305@gmail.com>
2018-05-15 14:27:07 +0100

    Added values for database configuration in config.php

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-  
+  'database' =>[
+    'connection' => 'mysql:host=127.0.0.1',
+    'name' => 'learning',
+    'username' => 'root',
+    'password' => '',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+    ]
+  ]
 ];
