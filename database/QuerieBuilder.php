5e3cc0341249374f5505d04a0e5fae47682ad8ff
Robert Marczak <robert.m.1305@gmail.com>
2018-05-15 13:04:56 +0100

    Added second argument for selectAll() in QuerieBuilder.php

--- database/QuerieBuilder.php
+++ database/QuerieBuilder.php
@@ -12,11 +12,11 @@ class QuerieBuilder
     $this->pdo = $pdo;
   }
 
-  public function selectAll(string $table)   //gets all data from the specified table
+  public function selectAll(string $table, $inToClass)   //gets all data from the specified table
   {
     $statement = $this->pdo->prepare("select * from {$table}");
     $statement->execute();
-    return $statement->fetchAll(PDO::FETCH_CLASS, 'User');
+    return $statement->fetchAll(PDO::FETCH_CLASS, $inToClass);
 
   }
 }
