e9e5eb838115e54b3f3b1258edf25f5d87f759cc
Robert Marczak <robert.m.1305@gmail.com>
2018-05-17 12:15:15 +0100

    Created contact.view.php

---
+++ views/contact.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+
+    <div class="navbar">
+      <a href="index.php" class="navitem">Home</a>
+      <a href="contact.php" class="navitem">Contact</a>
+      <a href="about.php" class="navitem">About Us</a>
+    </div>
+
+    <head>
+      Contact Us
+    </head>
+  </body>
+</html>
