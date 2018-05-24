8f263aaddf69ca5f548685aeb373758f5a682f0b
Robert Marczak <robert.m.1305@gmail.com>
2018-05-24 12:52:21 +0100

    Created routes.php

---
+++ routes/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$router->define([
+  '' => 'controllers/index.php',
+  'about' => 'controllers/about.php',
+  'about/something' => 'controllers/about-something.php',
+  'contact' => 'controllers/contact.php',
+]);
