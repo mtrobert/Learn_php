e8f399d2e314b49b074b8ea004f74a219b79d93d
Robert Marczak <robert.m.1305@gmail.com>
2018-06-04 12:47:43 +0100

    Edited paths for the keys in the routes array

--- routes/routes.php
+++ routes/routes.php
@@ -2,8 +2,8 @@
 
 
 $router->define([
-  '' => 'controllers/index.php',
-  'about' => 'controllers/about.php',
-  'about/something' => 'controllers/about-something.php',
-  'contact' => 'controllers/contact.php',
+  'php/learn_php' => 'index.php',
+  'php/learn_php/about' => 'controllers/about.php',
+  'php/learn_php/about/something' => 'controllers/about-something.php',
+  'php/learn_php/contact' => 'controllers/contact.php',
 ]);
