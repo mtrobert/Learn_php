ac0b0474eb4b28e0e8d9cc4c9641a60e5b07cc8d
Robert Marczak <robert.m.1305@gmail.com>
2018-06-04 12:56:47 +0100

    Added direct() method to the class

--- routes/Router.php
+++ routes/Router.php
@@ -11,4 +11,16 @@ class Router
     $this->routes = $routes;
   }
 
+  public function direct($uri)
+  {
+    if (array_key_exists($uri, $this->routes)){
+
+      return $this->routes[$uri];
+
+    }
+
+      throw new Exception("No route found for this URI.");
+
+
+  }
 }
