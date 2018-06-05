ede16d7c0f680f1153bef12932666fcab4179180
Robert Marczak <robert.m.1305@gmail.com>
2018-06-05 12:52:31 +0100

    Replaced hard coded html with views/partials files in all view files

--- views/about.view.php
+++ views/about.view.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body>
+<?php require 'partials/head.php'; ?>
 
-    <div class="navbar">
-      <a href="/php/learn_php/" class="navitem">Home</a>
-      <a href="/php/learn_php/contact" class="navitem">Contact</a>
-      <a href="/php/learn_php/about" class="navitem">About Us</a>
-    </div>
+  <h1>ABOUT US</h1>
 
-    <head>
-      About Us
-    </head>
-  </body>
-</html>
+<?php require 'partials/footer.php'; ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-    <body>
-      <div class="navbar">
-        <a href="/php/learn_php/" class="navitem">Home</a>
-        <a href="/php/learn_php/contact" class="navitem">Contact</a>
-        <a href="/php/learn_php/about" class="navitem">About Us</a>
-      </div>
+<?php require 'partials/head.php'; ?>
+
+      <h1>HOME</h1>
+
       <?php require 'views/Database_example.view.php'; ?>
 
-    </body>
-</html>
+
+<?php require 'partials/footer.php'; ?>
