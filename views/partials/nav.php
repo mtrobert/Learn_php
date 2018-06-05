70e21e12b8d74bf6a95627f14c4546e838307f06
Robert Marczak <robert.m.1305@gmail.com>
2018-06-05 12:28:37 +0100

    Created nav.php in views/partials

---
+++ views/partials/nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar">
+  <a href="/php/learn_php/" class="navitem">Home</a>
+  <a href="/php/learn_php/contact" class="navitem">Contact</a>
+  <a href="/php/learn_php/about" class="navitem">About Us</a>
+</nav>
