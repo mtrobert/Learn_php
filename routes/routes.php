1dc4334204788d7cd732480525ecc60c21296164
Robert Marczak <robert.m.1305@gmail.com>
2018-06-07 11:17:43 +0100

    Modified route for about-something to match folder structure in routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,6 @@
 $router->define([
   'php/learn_php' => 'index.php',
   'php/learn_php/about' => 'controllers/about.php',
-  'php/learn_php/about/something' => 'controllers/about-something.php',
+  'php/learn_php/about-something' => 'controllers/about-something.php',
   'php/learn_php/contact' => 'controllers/contact.php',
 ]);
