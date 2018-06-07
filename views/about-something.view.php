0d2cab1176af71d67d5f07a63e50197e510efd61
Robert Marczak <robert.m.1305@gmail.com>
2018-06-07 11:15:15 +0100

    Replaced hard code html code for views/partials/ files in about_something.php

--- views/about-something.view.php
+++ views/about-something.view.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body>
+<?php require 'views/partials/head.php'; ?>
+
     <h1>About Something</h1>
-  </body>
-</html>
+    
+<?php require 'views/partials/footer.php'; ?>
