f97104a385040720a168275953c425a6bea7d7e1
Robert Marczak <robert.m.1305@gmail.com>
2018-06-09 19:07:23 +0100

    Added form to the views.contact.php

--- views/contact.view.php
+++ views/contact.view.php
@@ -2,4 +2,15 @@
 
     <h1>CONTACT US</h1>
 
+    <form method="GET" action="names">
+      Name: <input type="text" name="name"></br>
+      Last Name: <input type="text" name="last_name"></br>
+      Age: <input type="text" name="age"></br>
+      Learning: <select name="learning">
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+      <button type="submit">Submit</button>
+    </form>
+
 <?php require 'partials/footer.php'; ?>
