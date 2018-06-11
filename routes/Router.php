863f654295fb3f61e63d02602e6e1b8b354e77af
Robert Marczak <robert.m.1305@gmail.com>
2018-06-11 11:23:02 +0100

    Added get() and post() methods in Router.php

--- routes/Router.php
+++ routes/Router.php
@@ -5,20 +5,27 @@
 class Router
 {
   protected $routes = [
-    'get' => [],
-    'post' => []
+    'GET' => [],
+    'POST' => []
   ];
 
-  public function define($routes)
+  public function get($uri, $controller)
   {
-    $this->routes = $routes;
+    $this->routes['GET'][$uri] = $controller;
   }
 
-  public function direct($uri)
-  {
-    if (array_key_exists($uri, $this->routes)){
 
-      return $this->routes[$uri];
+  public function post($uri, $controller)
+  {
+    $this->routes['POST'][$uri] = $controller;
+  }
+
+
+  public function direct($uri, $requestType)
+  {
+    if (array_key_exists($uri, $this->routes[$requestType])){
+
+      return $this->routes[$requestType][$uri];
 
     }
 
