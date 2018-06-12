a0e29c8035d03f86cfcecbaba9a1097ee5782a55
Robert Marczak <robert.m.1305@gmail.com>
2018-06-12 11:25:24 +0100

    Added h1 tag and the method to insert data to a database

--- controllers/add_user.php
+++ controllers/add_user.php
@@ -1,1 +1,18 @@
+<?php require 'views/partials/head.php'; ?>
+
+<h1>The data has been successfully added to a database:</h1>
+
 <?php
+// var_dump($_POST)
+
+$query->insert('users', [
+
+    'name' => $_POST['name'],
+    'age' => $_POST['age'],
+    'learning' => $_POST['learning']
+
+]);
+
+
+
+?>
