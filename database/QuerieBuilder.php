069191e27ccd6af679a789ee5baf0fa8e8aeac7d
Robert Marczak <robert.m.1305@gmail.com>
2018-06-13 11:37:12 +0100

    Added insert method in QuerieBuilder.php

--- database/QuerieBuilder.php
+++ database/QuerieBuilder.php
@@ -12,11 +12,36 @@ class QuerieBuilder
     $this->pdo = $pdo;
   }
 
+
+
   public function selectAll(string $table, $inToClass)   //gets all data from the specified table
   {
     $statement = $this->pdo->prepare("select * from {$table}");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_CLASS, $inToClass);
+  }
+
+
+
+  public function insert($tableName, array $parameters)
+  {
+
+    $sql = sprintf('insert into %s (%s ) values(%s )',
+        $tableName,
+        implode(', ', array_keys($parameters)),
+        ':' . implode(', :', array_keys($parameters))
+      );
+try {
+
+  $statement = $this->pdo->prepare($sql);
+  $statement->execute($parameters);
+
+} catch (Exception $e) {
+
+  die('Whoops something went wrong');
+
+}
+
 
   }
 }
