66b5aab7aafef750cb88ff8266e20c7a12ca6f3e
Robert Marczak <robert.m.1305@gmail.com>
2018-06-14 10:40:24 +0100

    Removed all require statement that require a class file

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,11 +3,6 @@
 $config = require 'core/config.php';
 
 
-require 'routes/Router.php';
-require 'core/Request.php';
-require 'database/ConnectToADatabase.php';
-require 'database/QuerieBuilder.php';
-
 return new QuerieBuilder(
   ConnectToADatabase::ConnectToDb($config['database'])
 );
