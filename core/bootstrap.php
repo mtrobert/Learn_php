e4283017cab673db7e8d259f392b05eb4ce100d9
Robert Marczak <robert.m.1305@gmail.com>
2018-06-15 20:49:16 +0100

    Replaced variable and return statement with DI container method bind()

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
-$config = require 'core/config.php';
+App::bind('config', require 'core/config.php');
+
+// $config = require 'core/config.php';
+
+// die(var_dump(App::get('config')));
 
 
-return new QuerieBuilder(
-  ConnectToADatabase::ConnectToDb($config['database'])
+App::bind('database', new QuerieBuilder(
+  ConnectToADatabase::ConnectToDb(App::get('config')['database']))
 );
+
+// return new QuerieBuilder(
+//   ConnectToADatabase::ConnectToDb($config['database'])
+// );
+// die(var_dump(App::get('database')));
